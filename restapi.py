af32c8a21acd2e092d6ccb37c8ebac06ada379ae
antoskuu <antoning.Antonin@gmail.com>
2025-10-23 19:54:20 +0200

    added delete endpoint

--- restapi.py
+++ restapi.py
@@ -45,7 +45,7 @@ async def get_cart(user_id):
 
 
 @app.post("/cart")
-async def add_to_cart(id, title, user_id):
+async def add_to_cart(id: int, title: str, user_id: str):
     db = read_db()
     users = db.get("users", {})
     if user_id not in users:
@@ -58,3 +58,17 @@ async def add_to_cart(id, title, user_id):
 
     write_db(db)
     return {"message": "Item added", "cart": cart}
+
+
+@app.delete("/cart")
+async def remove_from_cart(id: int, user_id: str):
+    db = read_db()
+    users = db.get("users", {})
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+    cart = users[user_id]["cart"]
+    cart = [item for item in cart if not (item["id"] == id)]
+    users[user_id]["cart"] = cart
+    db["users"] = users
+    write_db(db)
+    return {"message": "Item removed", "cart": cart}
